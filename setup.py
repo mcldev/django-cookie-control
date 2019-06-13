451888e969b74783945d1d4245a158e0829ce0a9
carderm <michael.carder@michaelcarder.co.uk>
2019-06-13 21:57:11 +1000

    Update template and version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_cookie_control',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
